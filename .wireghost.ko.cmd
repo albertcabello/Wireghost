87e6ddcf59458a464fb70ab680820aac81983b9c
Alberto Cabello <albert.a.cabello@gmail.com>
2017-08-15 23:03:07 -0400

    Cleared files

--- .wireghost.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/media/sf_Wireghost/wireghost.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /media/sf_Wireghost/wireghost.ko /media/sf_Wireghost/wireghost.o /media/sf_Wireghost/wireghost.mod.o
