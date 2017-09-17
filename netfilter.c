eae385c818c6e99b03272573107854dad25bc0a1
Alberto Cabello <albert.a.cabello@gmail.com>
2017-09-17 17:59:40 -0400

    Added conditional that will send an additional packet when true.

--- netfilter.c
+++ netfilter.c
@@ -1,26 +1,41 @@
-#include<linux/kernel.h>
-#include<linux/module.h>
-#include<linux/netfilter.h>
-#include<linux/netfilter_ipv4.h>
-#include<linux/ip.h>
-#include<linux/tcp.h>
 #include<net/ip.h>
 #include<net/tcp.h>
+#include<linux/ip.h>
+#include<linux/tcp.h>
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/proc_fs.h>
+#include<linux/seq_file.h>
+#include<linux/netfilter.h>
+#include<linux/etherdevice.h>
+#include<linux/netfilter_ipv4.h>
 #include "dictionary.h" 
+
+#define procfs_name "wireghost"
 
 // Parameters to be passed to the module
 static char * keyToReplace;
 static char * replacementForKey;
 
 // Paramter declarations 
-module_param(keyToReplace, charp, 0);
-module_param(replacementForKey, charp, 0);
+module_param(keyToReplace, charp, S_IRUGO | S_IWUSR);
+module_param(replacementForKey, charp, S_IRUGO | S_IWUSR);
 
 //Sequence and acknowledgement tables
 entry* seqTable[HASH_TABLE_SIZE];
 entry* ackTable[HASH_TABLE_SIZE];
 
-/* Converts four IP octets to an integer that can be used for comparison */
+static struct nf_hook_ops netfilter_ops_in; //NF_INET_PRE_ROUTING
+static struct nf_hook_ops netfilter_ops_out; //NF_INET_POST_ROUTING
+
+
+//Procfs structures
+struct proc_dir_entry *proc_file_entry; 
+static const struct file_operations proc_file_fops = {
+	.owner = THIS_MODULE,
+	.read = seq_read,
+};
+
 static inline int ipToInt(int a, int b, int c, int d) {
 	/* IP to int is first octet * 256^3 + second octet * 256^2 + third octet * 256 + fourth octet */
 	//return (a * 16777216) + (b * 65536) + (c * 256) + d;
@@ -54,22 +69,39 @@ int payloadFind(char* payload, const char* key, const char* replacement) {
 	return seen; //Return replacements 
 }
 
-static struct nf_hook_ops netfilter_ops_in; //NF_INET_PRE_ROUTING
-static struct nf_hook_ops netfilter_ops_out; //NF_INET_POST_ROUTING
-//Modify this function how you please to change what happens to incoming packets
-unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_state *state) {
-	/* TCP and IP headers */
+/* Update TCP and IP checksums for a given skb*/
+/* THIS DOESNT WORK ON NON LINEAR SKB */
+void fixChecksums(struct sk_buff * skb) {
 	struct tcphdr *tcph = tcp_hdr(skb);
 	struct iphdr *iph = ip_hdr(skb);
+	int tcp_len;
+	tcp_len = skb->len - 4 * iph->ihl;
+	iph->ttl += 1;
+	ip_send_check(iph);
+	tcph->check = 0;
+	tcph->check = tcp_v4_check(tcp_len, iph->saddr, iph->daddr, csum_partial((char *)tcph, tcp_len, 0));
+	if (!skb) {
+		printk("NETFILTER.C: SKB IS NULL FOR SOME REASON\n");
+	}
+
+}
+
+//Modify this function how you please to change what happens to incoming packets
+unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_state *state) {
+	struct sk_buff *nskb;
+	/* TCP, IP, and Ethernet headers */
+	struct tcphdr *ntcph, *tcph = tcp_hdr(skb);
+	struct iphdr *iph = ip_hdr(skb);
+	struct ethhdr *nethh, *ethh = eth_hdr(skb);
 	/* Structure that will be assigned to the seq and ack tables when needed */
 	struct entry t;
 	/* char * to end of payload, beginning of payload and an iterator */
-	unsigned char *tail, *user_data, *it;
-	/* char * to store the payload */
+	unsigned char *tail, *user_data, *it, *et;
+	/* char * to store the payload and the second packet payload */
 	char *payload;
 	/* Length of original payload, length of tcp header, number of replacements done to payload
 	 * the sequence and acknowledgement offsets, multipurpose iterator (for loops, while, etc) */
-	int lenOrig, tcp_len, replacements, offset, i;
+	int lenOrig, replacements, offset, i;
 	/* Source port, destination port and the ip header length */
 	__u16 sport, dport, ip_len;
 	/* Source IP address and destination IP address */
@@ -82,6 +114,10 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 	/* Ignore the skb if it is empty */
 	if (!skb)
 		return NF_ACCEPT;
+
+	if (iph->protocol == IPPROTO_ICMP) {
+		printk("NETFILTER.C: Ping packet incoming\n");
+	}
 
 	/* Convert the source IP address (saddr), source port (sport), 
 	   destination IP address (daddr), and destination port (dport)
@@ -109,6 +145,59 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 		}
 		payload[lenOrig] = '\0';
 
+		/*STORY TIME!!!!
+		The following if statement does in fact send packets, hooray!
+		The problem is, it sends them to itself and the packets never 
+		leave this computer, not hooray! I can get the packets to leave this
+		computer by removing the ethernet header though. "But Alberto, if removing
+		the ethernet header works, why don't you do it?" Ah my friend, if I do remove it
+		then the packets go to the other computers, but because there's no mac address
+		A bunch of the packet is cut off because the computer assumes that the beginning
+		of the packet is the mac addresses and the protocol, what do I do now?
+		I don't know.
+		*/
+
+		/* Begin sending of duplicate packet*/
+		if (false) { //Change condition to whatever
+			//printk("NETFILTER.C: Sending duplicate packet\n");
+
+			/* ALlocate a socket buffer and assign it to be a copy of the
+			one passed to us by netfilter */
+			nskb = alloc_skb(skb->len, GFP_ATOMIC);
+			nskb = pskb_copy(skb, GFP_ATOMIC);
+
+			/*pskb_copy does not give us the ethernet header, so create it */
+			nethh = eth_hdr(nskb);
+
+			/*memcpy to the new skb's ethernet header the destination
+			address in the original skb, do the same for the source */
+			memcpy(nethh->h_dest, ethh->h_dest, ETH_ALEN);
+			memcpy(nethh->h_source, ethh->h_source, ETH_ALEN);
+
+			//printk("NETFILTER.C: SOURCE MAC: %x:%x:%x", nethh->h_source[3], nethh->h_source[4], nethh->h_source[5]);
+			//printk(" DEST MAC: %x:%x:%x\n", nethh->h_dest[3], nethh->h_dest[4], nethh->h_dest[5]);
+
+			/*Assign the ethernet packets protocol (ETH_P_IP - 0x0800 which is IPv4) */
+			nethh->h_proto = ETH_P_IP;
+
+			/* Just to differentiate the duplicates easier, set the first 3 chars to capital H */
+			ntcph = tcp_hdr(nskb);
+			user_data = (unsigned char *)((unsigned char *)ntcph + (ntcph->doff * 4));
+			memcpy(user_data, "HHH", 3);
+
+			//dev_hard_header(nskb, skb->dev, ETH_P_IP, nethh->h_dest, nethh->h_source, ETH_ALEN);
+
+			/* The next four lines accomplish the same thing as
+			the dev_hard_header function, neither of which work right */
+			et = skb_push(nskb, ETH_HLEN);
+			memcpy(et, nethh->h_dest, ETH_ALEN);
+			memcpy(et+ETH_ALEN, nethh->h_source, ETH_ALEN);
+			nskb->protocol = nethh->h_proto = ntohs(ETH_P_IP);
+
+			i = dev_queue_xmit(nskb);
+
+		} /* Duplicate packet has now been sent*/
+
 		//printk("NETFILTER.C: DATA OF ORIGINAL SKB: %s\n", payload);
 
 		/* If keyToReplace or replacementForKey weren't passed in, don't change the packet
@@ -116,7 +205,7 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 		if (!keyToReplace || !replacementForKey) {
 			return NF_ACCEPT;
 		}
-
+		
 		/* Change the payload data stored in char * payload */
 		replacements = payloadFind(payload, keyToReplace, replacementForKey);
 
@@ -167,7 +256,7 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 		/* Change the size of the skb so that it allows for larger payloads
 		   may fail if the changed payload is shorter */
 		if(pskb_expand_head(skb, 0, strlen(payload)-skb_tailroom(skb), GFP_ATOMIC)) {
-			//printk("Sorry, we couldn't expand the skb, you'll just have to accept it\n");
+			printk("Sorry, we couldn't expand the skb, you'll just have to accept it\n");
 			return NF_ACCEPT;
 		}
 
@@ -188,13 +277,8 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 		ip_len += strlen(payload)-lenOrig;
 		iph->tot_len = htons(ip_len);
 
-		/* Update TCP and IP checksums */
-		/* THIS DOESNT WORK ON NON LINEAR SKB */
-		tcp_len = skb->len - 4 * iph->ihl;
-		iph->ttl += 1;
-		ip_send_check(iph);
-		tcph->check = 0;
-		tcph->check = tcp_v4_check(tcp_len, iph->saddr, iph->daddr, csum_partial((char *)tcph, tcp_len, 0));
+		/* Update TCP and IP Checksums */
+		fixChecksums(skb);
 
 	}
 	kfree(payload);
@@ -233,6 +317,7 @@ unsigned int out_hook(void *priv, struct sk_buff * skb, const struct nf_hook_sta
 }
 
 int init_module() {
+	printk("NETFILTER.C: Installing module wireghost\n");
 	netfilter_ops_in.hook = in_hook;
 	netfilter_ops_in.pf = PF_INET;
 	netfilter_ops_in.hooknum = NF_INET_PRE_ROUTING;
@@ -245,10 +330,23 @@ int init_module() {
 
 	nf_register_hook(&netfilter_ops_in);
 	nf_register_hook(&netfilter_ops_out);
+
+	proc_file_entry = proc_create("wireghost", 0, NULL, &proc_file_fops);
+	if (proc_file_entry == NULL) {
+		return -ENOMEM;
+	}
+
 	return 0;
 }
 
 void cleanup_module() {
 	nf_unregister_hook(&netfilter_ops_in);
 	nf_unregister_hook(&netfilter_ops_out);
+
+	proc_remove(proc_file_entry);
 }
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alberto Cabello");
+MODULE_DESCRIPTION("A packet modifier that maintains the TCP connection");
--- wireghost.mod.c
+++ wireghost.mod.c
@@ -22,14 +22,18 @@ __attribute__((section("__versions"))) = {
 	{ 0x754d539c, __VMLINUX_SYMBOL_STR(strlen) },
 	{ 0x2124474, __VMLINUX_SYMBOL_STR(ip_send_check) },
 	{ 0xe4a30d0e, __VMLINUX_SYMBOL_STR(nf_register_hook) },
+	{ 0x55dee550, __VMLINUX_SYMBOL_STR(seq_read) },
+	{ 0x27cb7ea0, __VMLINUX_SYMBOL_STR(proc_remove) },
 	{ 0xb97a8d7, __VMLINUX_SYMBOL_STR(param_ops_charp) },
 	{ 0x2e2b40d2, __VMLINUX_SYMBOL_STR(strncat) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0x9166fada, __VMLINUX_SYMBOL_STR(strncpy) },
 	{ 0x1e6d26a8, __VMLINUX_SYMBOL_STR(strstr) },
 	{ 0x61651be, __VMLINUX_SYMBOL_STR(strcat) },
 	{ 0xe16bca7a, __VMLINUX_SYMBOL_STR(pskb_expand_head) },
 	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
 	{ 0x910e0dc, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x55da9811, __VMLINUX_SYMBOL_STR(proc_create_data) },
 	{ 0xcf8262b6, __VMLINUX_SYMBOL_STR(nf_unregister_hook) },
 	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
 	{ 0x69acdf38, __VMLINUX_SYMBOL_STR(memcpy) },
@@ -43,4 +47,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "CCB4AD9B966C6B84AC69DF6");
+MODULE_INFO(srcversion, "E2ED9E4F0C03EA78F7E3B1D");
