e7a780ca6b269581545463515874c5e24d927553
Alberto Cabello <albert.a.cabello@gmail.com>
2017-09-20 22:55:29 -0400

    Added sysctl support, packet injection causes arpstorm (which is progress)

--- netfilter.c
+++ netfilter.c
@@ -4,22 +4,22 @@
 #include<linux/tcp.h>
 #include<linux/kernel.h>
 #include<linux/module.h>
-#include<linux/proc_fs.h>
-#include<linux/seq_file.h>
+#include<linux/sysctl.h>
 #include<linux/netfilter.h>
 #include<linux/etherdevice.h>
 #include<linux/netfilter_ipv4.h>
 #include "dictionary.h" 
 
-#define procfs_name "wireghost"
+#define MAX_KEY_LENGTH 400
+#define MAX_REPLACEMENT_LENGTH 400
 
 // Parameters to be passed to the module
-static char * keyToReplace;
-static char * replacementForKey;
+static char keyToReplace[MAX_KEY_LENGTH];
+static char replacementForKey[MAX_REPLACEMENT_LENGTH];
 
-// Paramter declarations 
-module_param(keyToReplace, charp, S_IRUGO | S_IWUSR);
-module_param(replacementForKey, charp, S_IRUGO | S_IWUSR);
+//Parameter declarations, rw-r--r-- permissions
+module_param_string(keyToReplace, keyToReplace, MAX_KEY_LENGTH, 0644);
+module_param_string(replacementForKey, replacementForKey, MAX_REPLACEMENT_LENGTH, 0644);
 
 //Sequence and acknowledgement tables
 entry* seqTable[HASH_TABLE_SIZE];
@@ -28,17 +28,52 @@ entry* ackTable[HASH_TABLE_SIZE];
 static struct nf_hook_ops netfilter_ops_in; //NF_INET_PRE_ROUTING
 static struct nf_hook_ops netfilter_ops_out; //NF_INET_POST_ROUTING
 
-
-//Procfs structures
-struct proc_dir_entry *proc_file_entry; 
-static const struct file_operations proc_file_fops = {
-	.owner = THIS_MODULE,
-	.read = seq_read,
+//Sysctl support structures
+//Leaf nodes in wireghost directory
+static struct ctl_table wireghost_table[] = {
+	{
+		.procname	= "key",
+		.data		= &keyToReplace,
+		.maxlen		= 400,
+		.mode 		= 0644,
+		.proc_handler 	= proc_dostring
+	},
+	{
+		.procname 	= "replacement",
+		.data		= &replacementForKey,
+		.maxlen		= 400,
+		.mode		= 0644,
+		.proc_handler 	= proc_dostring
+	},
+	{}
 };
 
+//Create wireghost directory with above leaf nodes
+static struct ctl_table wireghost_dir_table[] = {
+	{
+		.procname	= "wireghost",
+		.mode 		= 0555,
+		.child		= wireghost_table
+	},
+	{}
+};
+
+//Create wireghost directory under /proc/sys/net
+static struct ctl_table wireghost_root_table[] = {
+	{
+		.procname	= "net",
+		.mode		= 0555,
+		.child		= wireghost_dir_table
+	},
+	{}
+};
+
+static struct ctl_table_header *wireghost_table_header;
+//End of sysctl structures
+
+//Support function, converts IP addresses to intgers
 static inline int ipToInt(int a, int b, int c, int d) {
 	/* IP to int is first octet * 256^3 + second octet * 256^2 + third octet * 256 + fourth octet */
-	//return (a * 16777216) + (b * 65536) + (c * 256) + d;
 	/* Bit shifting is quicker than multiplication
 	 * 2^24 = 256^3, 2^16 = 256^2, 256 = 2^8 */
 	return (a << 24) + (b << 16) + (c << 8) + d;
@@ -86,22 +121,106 @@ void fixChecksums(struct sk_buff * skb) {
 
 }
 
+/*STORY TIME!!!!
+The following if statement does in fact send packets, hooray!
+The problem is, it sends them to itself and the packets never 
+leave this computer, not hooray! I can get the packets to leave this
+computer by removing the ethernet header though. "But Alberto, if removing
+the ethernet header works, why don't you do it?" Ah my friend, if I do remove it
+then the packets go to the other computers, but because there's no mac address
+A bunch of the packet is cut off because the computer assumes that the beginning
+of the packet is the mac addresses and the protocol, what do I do now?
+I don't know.
+*/
+
+//Causes arpstorm currently, which means its some seq/ack issue
+int injectNewPacket(struct sk_buff *orig, int saddr, int daddr, int lenOrig) {
+	struct entry t;
+	struct sk_buff *skb;
+	struct tcphdr *ntcph;
+	struct ethhdr *ethh = eth_hdr(orig);
+	unsigned char *user_data;
+	uint32_t seq, ack;
+	int offset;
+
+	/* Allocate a socket buffer and assign it to be a copy of the
+	one passed to us by netfilter */
+	skb = alloc_skb(orig->len, GFP_ATOMIC);
+	skb = pskb_copy(orig, GFP_ATOMIC);
+
+	/*Create pointer to tcp header*/
+	ntcph = tcp_hdr(skb);
+
+	//Copying the packet resets the ttl and fixChecksum() adds 1
+	//to it causing a ttl of 65
+	ip_hdr(skb)->ttl -= 1;
+
+	/*pskb_copy does not give us the ethernet header, so create it */
+//	nethh = eth_hdr(skb);
+
+//	/*memcpy to the new skb's ethernet header the destination
+//	address in the original skb, do the same for the source */
+//	memcpy(nethh->h_dest, ethh->h_dest, ETH_ALEN);
+//	memcpy(nethh->h_source, ethh->h_source, ETH_ALEN);
+//
+//	//printk("NETFILTER.C: SOURCE MAC: %x:%x:%x", nethh->h_source[3], nethh->h_source[4], nethh->h_source[5]);
+//	//printk(" DEST MAC: %x:%x:%x\n", nethh->h_dest[3], nethh->h_dest[4], nethh->h_dest[5]);
+//
+//	/*Assign the ethernet packets protocol (ETH_P_IP - 0x0800 which is IPv4) */
+//	nethh->h_proto = ETH_P_IP;
+
+//	memcpy(nethh, eth_hdr(orig), sizeof(ethhdr));
+
+	/* Just to differentiate the duplicates easier, set the first 3 chars to capital H */
+	ntcph = tcp_hdr(skb);
+	user_data = (unsigned char *)((unsigned char *)ntcph + (ntcph->doff * 4));
+	memcpy(user_data, "ZZZ", 3);
+
+	dev_hard_header(skb, orig->dev, ETH_P_IP, ethh->h_source, ethh->h_dest, orig->dev->addr_len);
+
+	/* The next four lines accomplish the same thing as
+	the dev_hard_header function, neither of which work right
+	et = skb_push(skb, ETH_HLEN);
+	memcpy(et, nethh->h_dest, ETH_ALEN);
+	memcpy(et+ETH_ALEN, nethh->h_source, ETH_ALEN);
+	skb->protocol = nethh->h_proto = ntohs(ETH_P_IP); */
+
+	
+//	seq = ntohl(ntcph->seq);
+//	seq += getVal(seqTable, saddr)->offset;
+//	ntcph->seq = htonl(seq);
+//
+//	ack = ntohl(ntcph->seq);
+//	ack += getVal(ackTable, saddr)->offset;
+//	ntcph->ack = htonl(seq);
+
+	offset = skb_tail_pointer(skb)-user_data;
+	t.ip = saddr;
+	t.offset = getVal(seqTable, saddr)->offset + offset;
+	storeVal(seqTable, t);
+	t.ip = daddr;
+	t.offset = getVal(ackTable, daddr)->offset - offset;
+	storeVal(ackTable, t);
+
+	fixChecksums(skb);
+
+	return dev_queue_xmit(skb);
+}
+	
 //Modify this function how you please to change what happens to incoming packets
 unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_state *state) {
-	struct sk_buff *nskb;
 	/* TCP, IP, and Ethernet headers */
-	struct tcphdr *ntcph, *tcph = tcp_hdr(skb);
+	struct tcphdr *tcph = tcp_hdr(skb);
 	struct iphdr *iph = ip_hdr(skb);
-	struct ethhdr *nethh, *ethh = eth_hdr(skb);
 	/* Structure that will be assigned to the seq and ack tables when needed */
 	struct entry t;
 	/* char * to end of payload, beginning of payload and an iterator */
-	unsigned char *tail, *user_data, *it, *et;
+	unsigned char *tail, *user_data, *it;
 	/* char * to store the payload and the second packet payload */
 	char *payload;
 	/* Length of original payload, length of tcp header, number of replacements done to payload
 	 * the sequence and acknowledgement offsets, multipurpose iterator (for loops, while, etc) */
-	int lenOrig, replacements, offset, i;
+	int lenOrig, replacements, offset;
 	/* Source port, destination port and the ip header length */
 	__u16 sport, dport, ip_len;
 	/* Source IP address and destination IP address */
@@ -145,64 +264,13 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 		}
 		payload[lenOrig] = '\0';
 
-		/*STORY TIME!!!!
-		The following if statement does in fact send packets, hooray!
-		The problem is, it sends them to itself and the packets never 
-		leave this computer, not hooray! I can get the packets to leave this
-		computer by removing the ethernet header though. "But Alberto, if removing
-		the ethernet header works, why don't you do it?" Ah my friend, if I do remove it
-		then the packets go to the other computers, but because there's no mac address
-		A bunch of the packet is cut off because the computer assumes that the beginning
-		of the packet is the mac addresses and the protocol, what do I do now?
-		I don't know.
-		*/
 
-		/* Begin sending of duplicate packet*/
-		if (false) { //Change condition to whatever
-			//printk("NETFILTER.C: Sending duplicate packet\n");
-
-			/* ALlocate a socket buffer and assign it to be a copy of the
-			one passed to us by netfilter */
-			nskb = alloc_skb(skb->len, GFP_ATOMIC);
-			nskb = pskb_copy(skb, GFP_ATOMIC);
-
-			/*pskb_copy does not give us the ethernet header, so create it */
-			nethh = eth_hdr(nskb);
-
-			/*memcpy to the new skb's ethernet header the destination
-			address in the original skb, do the same for the source */
-			memcpy(nethh->h_dest, ethh->h_dest, ETH_ALEN);
-			memcpy(nethh->h_source, ethh->h_source, ETH_ALEN);
-
-			//printk("NETFILTER.C: SOURCE MAC: %x:%x:%x", nethh->h_source[3], nethh->h_source[4], nethh->h_source[5]);
-			//printk(" DEST MAC: %x:%x:%x\n", nethh->h_dest[3], nethh->h_dest[4], nethh->h_dest[5]);
-
-			/*Assign the ethernet packets protocol (ETH_P_IP - 0x0800 which is IPv4) */
-			nethh->h_proto = ETH_P_IP;
-
-			/* Just to differentiate the duplicates easier, set the first 3 chars to capital H */
-			ntcph = tcp_hdr(nskb);
-			user_data = (unsigned char *)((unsigned char *)ntcph + (ntcph->doff * 4));
-			memcpy(user_data, "HHH", 3);
-
-			//dev_hard_header(nskb, skb->dev, ETH_P_IP, nethh->h_dest, nethh->h_source, ETH_ALEN);
-
-			/* The next four lines accomplish the same thing as
-			the dev_hard_header function, neither of which work right */
-			et = skb_push(nskb, ETH_HLEN);
-			memcpy(et, nethh->h_dest, ETH_ALEN);
-			memcpy(et+ETH_ALEN, nethh->h_source, ETH_ALEN);
-			nskb->protocol = nethh->h_proto = ntohs(ETH_P_IP);
-
-			i = dev_queue_xmit(nskb);
-
-		} /* Duplicate packet has now been sent*/
 
 		//printk("NETFILTER.C: DATA OF ORIGINAL SKB: %s\n", payload);
 
 		/* If keyToReplace or replacementForKey weren't passed in, don't change the packet
 		 * and if we're not changing anything, just accept it anyways */
-		if (!keyToReplace || !replacementForKey) {
+		if (!strlen(keyToReplace) || !strlen(replacementForKey)) {
 			return NF_ACCEPT;
 		}
 		
@@ -226,6 +294,10 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 			storeVal(seqTable, t);
 		}
 
+		if (strstr(payload, "hhh")) { //Change condition to whatever
+			injectNewPacket(skb, saddr, daddr, lenOrig);
+		}
+
 		/* Change sequence number from network to host, add offset, back to network */
 		seq = ntohl(tcph->seq);
 		seq += getVal(seqTable, saddr)->offset;
@@ -235,6 +307,7 @@ unsigned int in_hook(void *priv, struct sk_buff * skb, const struct nf_hook_stat
 		ack = ntohl(tcph->ack_seq);
 		ack += getVal(ackTable, saddr)->offset;
 		tcph->ack_seq = htonl(ack);
+
 
 		/* If the length of the outgoing packet is different from the original payload
 		 * the acknowledgement and sequence numbers will be off. This will cause the two 
@@ -330,9 +403,9 @@ int init_module() {
 
 	nf_register_hook(&netfilter_ops_in);
 	nf_register_hook(&netfilter_ops_out);
-
-	proc_file_entry = proc_create("wireghost", 0, NULL, &proc_file_fops);
-	if (proc_file_entry == NULL) {
+	
+	wireghost_table_header = register_sysctl_table(wireghost_root_table);
+	if (!wireghost_table_header) {
 		return -ENOMEM;
 	}
 
@@ -343,7 +416,7 @@ void cleanup_module() {
 	nf_unregister_hook(&netfilter_ops_in);
 	nf_unregister_hook(&netfilter_ops_out);
 
-	proc_remove(proc_file_entry);
+	unregister_sysctl_table(wireghost_table_header);
 }
 
 
--- wireghost.mod.c
+++ wireghost.mod.c
@@ -19,25 +19,28 @@ __used
 __attribute__((section("__versions"))) = {
 	{ 0xd2901226, __VMLINUX_SYMBOL_STR(module_layout) },
 	{ 0x84f43e8d, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x4489da02, __VMLINUX_SYMBOL_STR(register_sysctl_table) },
 	{ 0x754d539c, __VMLINUX_SYMBOL_STR(strlen) },
 	{ 0x2124474, __VMLINUX_SYMBOL_STR(ip_send_check) },
 	{ 0xe4a30d0e, __VMLINUX_SYMBOL_STR(nf_register_hook) },
-	{ 0x55dee550, __VMLINUX_SYMBOL_STR(seq_read) },
-	{ 0x27cb7ea0, __VMLINUX_SYMBOL_STR(proc_remove) },
-	{ 0xb97a8d7, __VMLINUX_SYMBOL_STR(param_ops_charp) },
+	{ 0xebc22bc0, __VMLINUX_SYMBOL_STR(param_ops_string) },
 	{ 0x2e2b40d2, __VMLINUX_SYMBOL_STR(strncat) },
 	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0x9166fada, __VMLINUX_SYMBOL_STR(strncpy) },
 	{ 0x1e6d26a8, __VMLINUX_SYMBOL_STR(strstr) },
+	{ 0x71828a66, __VMLINUX_SYMBOL_STR(proc_dostring) },
 	{ 0x61651be, __VMLINUX_SYMBOL_STR(strcat) },
+	{ 0xc2048649, __VMLINUX_SYMBOL_STR(__alloc_skb) },
+	{ 0xa06ff399, __VMLINUX_SYMBOL_STR(unregister_sysctl_table) },
 	{ 0xe16bca7a, __VMLINUX_SYMBOL_STR(pskb_expand_head) },
 	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+	{ 0xcfa218f, __VMLINUX_SYMBOL_STR(__pskb_copy_fclone) },
 	{ 0x910e0dc, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
-	{ 0x55da9811, __VMLINUX_SYMBOL_STR(proc_create_data) },
 	{ 0xcf8262b6, __VMLINUX_SYMBOL_STR(nf_unregister_hook) },
 	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
 	{ 0x69acdf38, __VMLINUX_SYMBOL_STR(memcpy) },
 	{ 0xe113bbbc, __VMLINUX_SYMBOL_STR(csum_partial) },
+	{ 0x24ad15ed, __VMLINUX_SYMBOL_STR(dev_queue_xmit) },
 	{ 0xe8914f18, __VMLINUX_SYMBOL_STR(skb_put) },
 };
 
@@ -47,4 +50,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "E2ED9E4F0C03EA78F7E3B1D");
+MODULE_INFO(srcversion, "77E5EE74F30C6ABADC44C78");
